a5543e574f67794996d5232dc0f4b81a9bbc24fc
Bogdan <58550245+Pinkavsky@users.noreply.github.com>
2020-11-20 15:33:13 +0200

    Add files via upload

---
+++ MobileDevelopment/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MobileDevelopment
+//
+//  Created by Bogdan Pinkavsky
+//  Copyright © 2020 Bogdan Pinkavsky. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
